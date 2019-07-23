9d06496c092f331480c95f1e8377ff4ac1186b89
Patrick <p@rickjobin.com>
2019-07-23 09:24:29 -0400

    Updates

---
+++ precache-manifest.e0b10a3c411dc9f90cd86861d94e4b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b760c48521e6d678d789957a1e1c5e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a13e31c4ade37cf091d3",
+    "url": "/monsters/static/css/main.69d7c878.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/monsters/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "a13e31c4ade37cf091d3",
+    "url": "/monsters/static/js/main.8dd9d6fc.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
